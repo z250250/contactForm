13cda8fcd40f7d5a61e082fe9d0250b63310e709
z2520 <z250abe@gmail.com>
2020-07-31 10:25:31 +0900

    お問い合わせフォーム

---
+++ contactfrom/insert.php
@@ -0,0 +1,32 @@
+
+ <?php
+mb_internal_encoding("utf8");
+
+$pdo = new PDO("mysql:dbname=abe;host=localhost;","root","root");
+
+$pdo->exec("insert into contactfrom(name,mail,age,comments)
+values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
+?>
+
+
+<!doctype HTML>
+<html lang="ja">
+
+    <head>
+        <meta charset="utf-8">
+        <title>お問い合わせフォーム</title>
+        <link rel="stylesheet" type="text/css" href="style2.css">
+    </head>
+    <body>
+    
+        <h1>お問い合わせフォーム</h1>
+        
+        <div class="confirm">
+        
+            <p>お問い合わせ有難うございました。<br>3営業以内に担当者よりご連絡差し上げます。
+            </p>
+        </div>
+        
+
+    </body>
+</html>
